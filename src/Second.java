14ed817cb1779c4250d55fcfe57775eff83f6e0d
lisme <lisme1@163.com>
2013-07-10 23:30:01 +0800

    first commit

---
+++ src/Second.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)Second.java
+ *
+ *
+ * @author 
+ * @version 1.00 2013/7/9
+ */
+
+public class Second {
+        
+    /**
+     * Creates a new instance of <code>Second</code>.
+     */
+    public Second() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("helo");
+    }
+}
