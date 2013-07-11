8f2ef6afdd39bd9917383828e2c6f3dfe391db05
lisme <lisme1@163.com>
2013-07-11 11:12:21 +0800

    delete useless files

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)Second.java
+ *
+ *
+ * @author 
+ * @version 1.00 2013/7/9
+ */
+
+public class Test {
+        
+    /**
+     * Creates a new instance of <code>Second</code>.
+     */
+    public Test() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("helo");
+    }
+}
